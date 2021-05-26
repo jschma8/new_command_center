742ef8e587125becd33cf053d7e2603637f92e85
unknown <jschma8@wgu.edu>
2021-05-26 17:06:38 -0400

    adding information to ReadME adding global prettier file adding Firebase to global/main.js

---
+++ README.md
@@ -0,0 +1,14 @@
+# Main Read ME file.
+
+Use this prettier file for standardization of look for all code.
+
+Style guides include:
+PEP8 for Python
+https://www.w3schools.com/js/js_conventions.asp for Javascript.
+
+Specific deviations will be noted below:
+Use single quotes whenever possible.
+Comments are only necessary if you need to make sure you understand something or if it is extremely complex.
+Plan for failure BECAUSE!
+Follow the Zen of Python as much as possible.
+DRY, YAGNI, SOLID should be standards not options.
--- globalVue/src/main.js
+++ globalVue/src/main.js
@@ -2,11 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import firebase from 'firebase/app'
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCgNif-sfiNmW14hRpF4Rgvx0qTn6r99LM',
+  authDomain: 'new-cc.firebaseapp.com',
+  projectId: 'new-cc',
+  storageBucket: 'new-cc.appspot.com',
+  messagingSenderId: '1091642304922',
+  appId: '1:1091642304922:web:837dcaae7513e067d1b223',
+  measurementId: 'G-2DT1SGBBTP',
+}
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
